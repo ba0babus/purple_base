efbf3fbb24d0f7fbaa27b607470880831b0e22df
Denis Uliankin <denis@Mac.lan>
2026-01-05 12:54:27 +0300

    Add some functions to currency converter

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,8 +5,24 @@ import (
 )
 
 func main() {
+
 	const eurToUsd float64 = 1.17
 	const usdToRub float64 = 81.15
 	const eurToRub = usdToRub * eurToUsd
 	fmt.Println("EUR to RUB rate:", eurToRub)
+	getUserInput()
+	calculateCurrency(5, "sosiska", "salfetka")
+}
+func getUserInput() (float64, float64) {
+	var value1, value2 float64
+	fmt.Print("Input some: ")
+	fmt.Scan(&value1)
+	fmt.Print("Input some2: ")
+	fmt.Scan(&value2)
+	return value1, value2
+}
+
+func calculateCurrency(someInt int, currency1 string, currency2 string) float64 {
+	result := 0.0
+	return result
 }
