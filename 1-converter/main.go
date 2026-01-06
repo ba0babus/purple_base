6066a5668e5684898f8f3fa1bda21ab0878f8b45
Denis <denis.uliankin@gmail.com>
2026-01-06 13:56:20 +0300

    Add some functions to currency converter

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,24 +5,94 @@ import (
 )
 
 func main() {
-
-	const eurToUsd float64 = 1.17
-	const usdToRub float64 = 81.15
-	const eurToRub = usdToRub * eurToUsd
-	fmt.Println("EUR to RUB rate:", eurToRub)
-	getUserInput()
-	calculateCurrency(5, "sosiska", "salfetka")
-}
-func getUserInput() (float64, float64) {
-	var value1, value2 float64
-	fmt.Print("Input some: ")
-	fmt.Scan(&value1)
-	fmt.Print("Input some2: ")
-	fmt.Scan(&value2)
-	return value1, value2
+	calculateCurrency(getUserInput())
 }
 
-func calculateCurrency(someInt int, currency1 string, currency2 string) float64 {
-	result := 0.0
-	return result
+func getUserInput() (string, float64, string) {
+	var fromCurrency, toCurrency string
+	var amount float64
+fromCurrencyLoop:
+	for { // выбираем исходную валюту
+		fmt.Printf("Choose the currency number from convert (e.g.: 2): \n")
+		fmt.Printf("USD = 1\nEUR = 2\nRUB = 3\n")
+		fmt.Scan(&fromCurrency)
+		switch fromCurrency {
+		case "1":
+			fromCurrency = "USD"
+			break fromCurrencyLoop
+		case "2":
+			fromCurrency = "EUR"
+			break fromCurrencyLoop
+		case "3":
+			fromCurrency = "RUB"
+			break fromCurrencyLoop
+		default:
+			fmt.Printf("Please, type a number from 1 to 3. Try again...\n\n\n")
+			continue fromCurrencyLoop
+		}
+	}
+
+amountLoop:
+	for { // выбираем количество
+		fmt.Printf("Enter the amout of currency to convert: ")
+		fmt.Scan(&amount)
+		if amount < 0 {
+			fmt.Printf("The amount of currency cannot be less than 0. Try again...\n\n\n")
+			continue amountLoop
+		} else {
+			break amountLoop
+		}
+	}
+toCurrencyLoop:
+	for { // выбираем исходную валюту
+		fmt.Printf("Choose the currency number to convert (e.g.: 2): \n")
+		fmt.Printf("USD = 1\nEUR = 2\nRUB = 3\n")
+		fmt.Scan(&toCurrency)
+		switch toCurrency {
+		case "1":
+			toCurrency = "USD"
+			break toCurrencyLoop
+		case "2":
+			toCurrency = "EUR"
+			break toCurrencyLoop
+		case "3":
+			toCurrency = "RUB"
+			break toCurrencyLoop
+		default:
+			fmt.Printf("Please, type a number from 1 to 3. Try again...\n\n\n")
+			continue toCurrencyLoop
+		}
+	}
+	return fromCurrency, amount, toCurrency
+}
+
+func calculateCurrency(fromCurrency string, amount float64, toCurrency string) {
+	const eurToUsd = 1.17
+	const eurToRub = 94.89
+	const rubToEur = 0.011
+	const rubToUsd = 0.012
+	const usdToEur = 0.85
+	const usdToRub = 81.15
+	var result float64
+
+	if fromCurrency == "USD" && toCurrency == "RUB" {
+		result = amount * usdToRub
+	} else if fromCurrency == "USD" && toCurrency == "EUR" {
+		result = amount * usdToEur
+	} else if fromCurrency == "RUB" && toCurrency == "USD" {
+		result = amount * rubToUsd
+	} else if fromCurrency == "RUB" && toCurrency == "EUR" {
+		result = amount * rubToEur
+	} else if fromCurrency == "EUR" && toCurrency == "RUB" {
+		result = amount * eurToRub
+	} else if fromCurrency == "EUR" && toCurrency == "USD" {
+		result = amount * eurToUsd
+	} else if fromCurrency == toCurrency {
+		result = amount
+	} else {
+		fmt.Println("Invalid currency pair")
+		return
+	}
+
+	fmt.Printf("Total: %0.2f\n", result)
 }
