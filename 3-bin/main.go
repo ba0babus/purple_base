377ac689d50e67dbd5843b976a97b5a3d3f96f7f
Denis <denis.uliankin@gmail.com>
2026-01-10 10:42:27 +0300

    Add bin app

---
+++ 3-bin/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type bin struct { // структура
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func newBin(id string, private bool, name string) *bin {
+	newAcc := &bin{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+
+	return newAcc
+}
+
+func main() {
+	var binList []string
+	someBin := newBin("123", true, "myNewBin")
+	fmt.Println(binList, someBin)
+}
